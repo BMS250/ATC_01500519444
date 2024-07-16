9cca0e7f313d266c42d289a55deae5ced2a7592f
Bola <bola.milad25@gmail.com>
2024-07-16 17:04:46 +0300

    For 07 - Home and Details Page

--- Web.DataAccess/Repository/Repository.cs
+++ Web.DataAccess/Repository/Repository.cs
@@ -26,10 +26,17 @@ namespace BookWeb.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
--- WebApplication1/Areas/Customer/Controllers/HomeController.cs
+++ WebApplication1/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using BookWeb.Models;
+using BookWeb.DataAccess.Repository.IRepository;
 
 namespace MyBookWeb.Areas.Customer.Controllers
 {
@@ -8,15 +9,24 @@ namespace MyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.ProductId == id, includeProperties: "Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()
