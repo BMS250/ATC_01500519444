b36a3a17e8fbfec7b8b279028f52657778452179
Bola <bola.milad25@gmail.com>
2024-07-25 02:45:27 +0300

    For 15 - User Management : 002 Display Company Name

--- Web.Utility/EmailSender.cs
+++ Web.Utility/EmailSender.cs
@@ -12,20 +12,21 @@ namespace BookWeb.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public string SendGridSecret { get; set; }
-        public EmailSender(IConfiguration _config)
-        {
-            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
-        }
+        //public string SendGridSecret { get; set; }
+        //public EmailSender(IConfiguration _config)
+        //{
+        //    SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        //}
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var client = new SendGridClient(SendGridSecret);
+            //var client = new SendGridClient(SendGridSecret);
 
-            var from = new EmailAddress("bola.milad25@gmail.com", "My Book Web");
-            var to = new EmailAddress(email);
-            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-            return client.SendEmailAsync(message);
+            //var from = new EmailAddress("bola.milad25@gmail.com", "My Book Web");
+            //var to = new EmailAddress(email);
+            //var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            //return client.SendEmailAsync(message);
+            return Task.CompletedTask;
         }
     }
 }
---
+++ WebApplication1/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BookWeb.DataAccess.Data;
+using BookWeb.DataAccess.Repository.IRepository;
+using BookWeb.Models;
+using BookWeb.Models.ViewModels;
+using BookWeb.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<ApplicationUser> users = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+            foreach (var user in users)
+            {
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+            return View(users);
+        }
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> users = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            return Json(new { data = users });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Delete successful" });
+        } 
+        #endregion
+    }
+}
