fc77a307716fb03ed7638a38b050b000c1c64488
Bola <bola.milad25@gmail.com>
2024-07-24 23:17:16 +0300

    For 14 - Deployment & Email (almost of it skipped)

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BookWeb.Utility;
 using Stripe;
+using BookWeb.DataAccess.DbInitializer;
+using WebBook.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -35,7 +37,7 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -57,9 +59,19 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
+SeedDatabase();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
